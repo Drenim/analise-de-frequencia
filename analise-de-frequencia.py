8a726a30550a436518aad70983559e59c443905c
Daniel Matos de Castro <daniel.matos.castro@gmail.com>
2018-02-19 22:05:24 -0300

    Criação das funções percentual_de_frequencia e constroi_relatorio

--- analise-de-frequencia.py
+++ analise-de-frequencia.py
@@ -2,7 +2,7 @@ import pprint
 from collections import Counter
 
 def obtem_linhas(path):
-    with open(path, mode="r", encoding="utf-8") as arquivo:
+    with open(path, mode="r") as arquivo:
         linhas = arquivo.readlines()
     
     return linhas
@@ -15,10 +15,28 @@ def conta_letras(linhas):
 
     return contador
 
+def percentual_de_frequencia(relatorio, contador):
+    total = sum(contador.values())
+    
+    for simbolo in contador:
+        relatorio[simbolo]["Freq. percentual"] = (contador[simbolo] * 100) / total
+
+
+def constroi_relatorio(contador):
+    relatorio = dict()
+
+    for simbolo in contador:
+        relatorio[simbolo] = dict()
+
+    percentual_de_frequencia(relatorio, contador)
+
+    return relatorio
+
 if __name__ == '__main__':
     linhas = obtem_linhas("o-alienista.txt")
     contador = conta_letras(linhas)
-    pprint.pprint(contador)
+    pprint.pprint(constroi_relatorio(contador))
+
 
 
 
